de42cc7ce41b47d457228b041013caa2f777bf26
AB-hijith <93638252+AB-hijith@users.noreply.github.com>
2021-11-09 09:40:51 +0530

    Program to swap 2 digits

--- swap.py
+++ swap.py
@@ -1,11 +1,11 @@
 a=int(input("First number="))
 b=int(input("Second number="))
 print("Before Swapping")
-print(a,b,sep="       ")
+print(a,b,sep="      ")
 a = a + b 
 b = a - b
 a = a - b
 print("After Swapping")
-print(a,b,sep="       ")
+print(a,b,sep="      ")
 
 
