3f5064ccc13f8f1304a24da7ca8fcbd73c7ca09d
Abhijith K S <93638252+AB-hijith@users.noreply.github.com>
2021-12-01 09:16:02 +0530

    Program to convert temperature

---
+++ degree.py
@@ -0,0 +1,9 @@
+a=int(input("Temperature in celsius:"))
+c=(a*9/5)+32
+print("Temperature in farenheit:",c)
+b=int(input("Temperature in farenheit:"))
+d=(b-32)*5/9
+print("Temperature in celsius:",d)
+
+
+
