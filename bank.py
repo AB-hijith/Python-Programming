2a300130ec7cf5d629471388af674084e5108eeb
Abhijith K S <93638252+AB-hijith@users.noreply.github.com>
2022-02-03 09:36:26 +0530

    Program to do bank calculations

---
+++ bank.py
@@ -0,0 +1,23 @@
+class account:
+	def __init__(self):
+		self.balance=0
+
+	def deposit(self):
+		amount=float(input("Enter amount to be Deposited: "))
+		self.balance += amount
+		print("\n Amount Deposited:",amount)
+
+	def withdraw(self):
+		amount = float(input("Enter amount to be Withdrawn: "))
+		if self.balance>=amount:
+			self.balance-=amount
+			print("\n You Withdrew:", amount)
+		else:
+			print("\n Insufficient balance ")
+
+	def display(self):
+		print("\n Net Available Balance=",self.balance)
+s =account()
+s.deposit()
+s.withdraw()
+s.display()
